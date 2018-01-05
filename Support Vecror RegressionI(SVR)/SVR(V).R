0cb632ec93bd4a27c39d4b1595069de26f9590b4
Sai Vikhyat <sparepal@uncc.edu>
2018-01-05 17:54:24 -0500

    Truth/bluff

---
+++ Support Vecror RegressionI(SVR)/SVR(V).R
@@ -0,0 +1,24 @@
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Fitting the SVR Model to the dataset
+#install.packages('e1071')
+library(e1071)
+regressor=svm(formula=Salary~.,
+              data=dataset,
+              type='eps-regression')
+# Predicting a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualising the SVR Model results
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (SVR Model)') +
+  xlab('Level') +
+  ylab('Salary')
